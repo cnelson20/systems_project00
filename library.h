2bd1b1b31e67fde7cd9c8bab70549b6dbb365828
cnelson20 <cjnelson2004@gmail.com>
2021-10-22 09:07:15 -0400

    created function headers

---
+++ library.h
@@ -0,0 +1,10 @@
+struct song_node **create_library();
+void add_song(struct song_node **lib, struct song_node *song);
+struct song_node *find_song(struct song_node **lib, char *name, char *artist);
+struct song_node *find_artist(struct song_node **lib, char *artist);
+void print_songs(struct song_node **lib, char letter);
+void print_artist_songs(struct song_node **lib, char *artist);
+void print_library(struct song_node **lib);
+struct song_node *shuffled_list(struct song_node **lib);
+void delete_song(struct song_node **lib, song_node *del);
+void free_lib(struct song_node **lib);
