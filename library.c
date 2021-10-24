46dbef79eb0b07852ebda0f81e175a6d12ee4a36
unknown <>
2021-10-23 21:44:32 -0400

    Fixes Shuffle

--- library.c
+++ library.c
@@ -3,6 +3,12 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define MIN(a,b) (a < b ? a : b)
+
+int listlength(struct song_node *s);
+struct song_node *shuffled_list_helper(struct song_node **lib, int length, struct song_node *shuf);
+int in_list(struct song_node *needle, struct song_node *haystack);
 
 struct song_node **create_library() {
   struct song_node **lib= malloc(27 * sizeof(*lib));
@@ -69,32 +75,42 @@ void print_library(struct song_node **lib){
   }
   printf("\n");
 }
-struct song_node *shuffled_list(struct song_node **lib, int length){
+
+struct song_node *shuffled_list_helper(struct song_node **lib, int length, struct song_node *shuf){
   int lengths[27];
-  int counter;
-  int sizecount;
-  int songnum;
-  struct song_node *rand;
-  sizecount = 0;
+  int counter, random;
+  int sizecount = 0;
+  struct song_node *rand_node, *chosen_node;
   for (counter = 0; counter < 27; counter++) {
-    sizecount += helper(lib[counter]);
+    sizecount += listlength(lib[counter]);
     lengths[counter] = sizecount;
   }
-  int random;
-  random = rand() % sizecount;
-  counter = 0;
-  while(random > lengths[counter]) {
-      counter++;
-  }
-  songnum = random - lengths[counter];
-  rand->name = lib[counter][songnum]->name;
-  rand->artist = lib[counter][songnum]->artist;
-  if(length = 0) {
-    next->NULL;
+  do {
+	random = rand() % sizecount;
+	//printf("Rand: %d\n",random);
+	  
+	counter = 0;
+	while (counter == 26 || random >= lengths[counter]) {
+	  counter++;
+	}
+	random -= (counter == 0 ? 0 : lengths[counter-1]);
+	chosen_node = lib[counter];
+	while (random--) {chosen_node = chosen_node->next;}
+  
+  } while (in_list(chosen_node,shuf));
+  
+  rand_node = malloc(sizeof(struct song_node));
+  strncpy(rand_node->name,chosen_node->name,SONG_ARTISTLENGTH - 1);
+  strncpy(rand_node->artist,chosen_node->artist,SONG_ARTISTLENGTH - 1);
+  if(length <= 1) {
+    rand_node->next = NULL;
   } else {
-    next->shuffled_list(lib,length--);
+    rand_node->next = shuffled_list_helper(lib,MIN(length - 1,sizecount-1),rand_node);
   }
-  return rand;
+  return rand_node;
+}
+struct song_node *shuffled_list(struct song_node **lib, int length) {
+ return shuffled_list_helper(lib,length,NULL);
 }
 
 void delete_song(struct song_node **lib, struct song_node *del){
@@ -116,11 +132,20 @@ struct song_node **clear_lib(struct song_node **lib){
   return lib;
 }
 
-int helper(struct song_node *s) {
+int listlength(struct song_node *s) {
   int l = 0;
   while (s) {
     s = s->next;
     l++;
   }
   return l;
+}
+int in_list(struct song_node *needle, struct song_node *haystack) {
+	while (haystack) {
+	  if (compare_songs(needle,haystack) == 0) {
+		return 1;
+	  }		  
+	  haystack = haystack->next;
+	}
+	return 0;
 }
